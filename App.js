310446ce50701a7439764bf6dee875d273892850
tulunadrockers <tulunadrockers2@gmail.com>
2026-04-27 17:06:50 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import './App.css'; 
+import Form from './Form'; 
+function App() { 
+return ( 
+<div className="App"> 
+<h1>React Form Validation</h1> 
+<Form /> 
+</div> 
+); 
+} 
+export default App; 
